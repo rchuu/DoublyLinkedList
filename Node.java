4219c6c09925c48c379bda8d0de46b7b67220912
Richard Chu <rchu1211@gmail.com>
2022-07-27 15:18:54 -0700

    first commit

---
+++ Node.java
@@ -0,0 +1,13 @@
+// package com.codingdojo.java.doublylinkedlist;
+
+public class Node {
+    public Integer value;
+    public Node previous;
+    public Node next;
+
+    public Node(Integer value) {
+        this.value = value;
+        this.previous = null;
+        this.next = null;
+    }
+}
